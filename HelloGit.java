3df99e7add109fe05ab339f4cdfa6b0ea7993e55
hbf0803 <gnnud0803@163.com>
2017-06-20 22:14:05 +0800

    modify HelloGit.java

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,7 @@
 public class HelloGit{
     public static void main(String[] args){
         System.out.println("Hello Git, this is my first try !");
+        System.out.println("I'm altering the file !");
     }
+    
 }
